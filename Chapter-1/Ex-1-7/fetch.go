0b474b14df8aaf7b1944ad90567b1c5f853120a8
r-r-dandekar <radhan.r.dandekar@gmail.com>
2026-09-11 07:46:17 +0530

    Ex. 1-7 to 1-9 : Fetching from a URL

---
+++ Chapter-1/Ex-1-7/fetch.go
@@ -0,0 +1,31 @@
+// Fetch prints the content found at a URL.
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+
+		var count int64
+		count = 1
+		for count > 0 {
+			count, err = io.Copy(os.Stdout, resp.Body)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+				resp.Body.Close()
+				os.Exit(1)
+			}
+		}
+		resp.Body.Close()
+	}
+}
---
+++ Chapter-1/Ex-1-9/fetch.go
@@ -0,0 +1,37 @@
+// Fetch prints the content found at a URL.
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		protocol := "http://"
+		if !strings.HasPrefix(url, protocol) {
+			url = protocol + url
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+
+		var count int64
+		count = 1
+		for count > 0 {
+			count, err = io.Copy(os.Stdout, resp.Body)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+				resp.Body.Close()
+				os.Exit(1)
+			}
+		}
+		fmt.Printf("\n\n\nStatus Code: %s\n", resp.Status)
+		resp.Body.Close()
+	}
+}
